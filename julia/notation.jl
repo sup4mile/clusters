f854af5f18e14174f92d25906cf952ed7db40e2d
BiancaYue <beyondyue817@gmail.com>
2021-09-11 21:06:38 -0500

    updated notation

---
+++ julia/notation.jl
@@ -0,0 +1,25 @@
+p_ciH:
+p_ciHx:
+P_iH:[nc * (i-1) + c] for c in 1:nc
+p_iF:[2nc*ni+i]
+p_iFx:
+y_ciH:
+y_ciHx:
+y_iH:
+y_iHx:
+y_iF:
+y_iFx:
+
+l_ci_vH:
+l_ci_vHx:
+l_i_vF:
+l_i_vFx:
+s_ciH:[6nc*ni+8ni+i]
+s_ciHx:[7nc*ni+8ni+i]
+
+Pi:
+Pix:
+Yi:
+Yix:
+Ei:
+Eix:
