38a41b2a985f1060290bc8fe8f4536ca2c8ed58d
BigRich0430 <rickshi1238@gmail.com>
2021-10-13 13:37:27 -0500

    deleted irrelavant files

--- julia/agg_out.jl
+++
@@ -1,4 +0,0 @@
-function agg_out(σ,ind_out)
-    y = sum((ind_out.^((σ-1)/σ))).^(σ/(σ-1))
-    return y
-end
--- julia/inv_demand.jl
+++
@@ -1,4 +0,0 @@
-function inv_demand(ρ,A,w,l,η)
-    p = ρ/(ρ-1).*w./(A.*l.^η)
-    return p
-end
